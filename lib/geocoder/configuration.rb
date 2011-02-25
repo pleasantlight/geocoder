da810604043965f99ae940fb16bf9512f335de4d
Noam Etzion-Rosenberg <noam@pleasantlight.com>
2011-02-25 21:10:41 +0200

    trying to replace Google Maps API with Waze

--- lib/geocoder/configuration.rb
+++ lib/geocoder/configuration.rb
@@ -5,6 +5,6 @@ module Geocoder
 end
 
 Geocoder::Configuration.timeout = 3
-Geocoder::Configuration.waze_api_key = 7d670924-136a-4b12-95e4-de90ed149c60
+Geocoder::Configuration.waze_api_key = "7d670924-136a-4b12-95e4-de90ed149c60"
 
 
