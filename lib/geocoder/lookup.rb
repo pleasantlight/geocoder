21fb2314a907dc6233b7dc362f672ec5dd334f9a
Noam Etzion-Rosenberg <noam@pleasantlight.com>
2011-02-26 08:55:32 +0200

    trying to replace Google Maps API with Waze

--- lib/geocoder/lookup.rb
+++ lib/geocoder/lookup.rb
@@ -9,9 +9,9 @@ module Geocoder
     #
     def coordinates(address)
       if (results = search(address)).size > 0
-        #place = results.first.geometry['location']
-        place = results.first["location"]
-        ['lat', 'lng'].map{ |i| place[i] }
+        place = results.first.geometry['location']
+        ['latitude', 'longitude'].map{ |i| place[i] }
+        # ['lat', 'lng'].map{ |i| place[i] }
       end
     end
 
@@ -19,7 +19,7 @@ module Geocoder
     # Query Google for the address of the given coordinates.
     #
     def address(latitude, longitude)
-      #if (results = search(latitude, longitude)).size > 0
+      if (results = search(latitude, longitude)).size > 0
         results.first.formatted_address
       end
     end
@@ -35,8 +35,8 @@ module Geocoder
       doc = parsed_response(args.join(","), args.size == 2)
       [].tap do |results|
         if doc
-          #doc['results'].each{ |r| results << Result.new(r) }
-          results << Result.new(doc)
+          doc['results'].each{ |r| results << Result.new(r) }
+          # results << Result.new(doc)
         end
       end
     end
@@ -58,17 +58,17 @@ module Geocoder
           "(see Geocoder::Configuration.timeout to set limit)."
       end
 
-      # case doc['status']; 
-      # when "OK"
+      case doc['status']; 
+      when "OK"
         puts "#{doc}"
         doc
-      # when "OVER_QUERY_LIMIT"
-      #   warn "Geocoding API error: over query limit."
-      # when "REQUEST_DENIED"
-      #   warn "Geocoding API error: request denied."
-      # when "INVALID_REQUEST"
-      #   warn "Geocoding API error: invalid request."
-      # end
+      when "OVER_QUERY_LIMIT"
+        warn "Geocoding API error: over query limit."
+      when "REQUEST_DENIED"
+        warn "Geocoding API error: request denied."
+      when "INVALID_REQUEST"
+        warn "Geocoding API error: invalid request."
+      end
     end
 
     ##
@@ -88,8 +88,8 @@ module Geocoder
         (reverse ? :latlng : :address) => query,
         :sensor => "false"
       }
-#      "http://maps.google.com/maps/api/geocode/json?" + params.to_query
-      "http://www.waze.co.il/WAS/mozi?q=#{CGI::escape query}&token=#{Geocoder::Configuration.waze_api_key}"
+      "http://maps.google.com/maps/api/geocode/json?" + params.to_query
+      # "http://www.waze.co.il/WAS/mozi?q=#{CGI::escape query}&token=#{Geocoder::Configuration.waze_api_key}"
     end
   end
 end
